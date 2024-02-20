acf4a7e1ee8b6b97ea467814619fef3c5d76b742
Naod Taddese <naodtds@gmail.com>
2024-02-21 01:01:45 +0300

    fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
+-- this creates the database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---blow statement creates user
+-- blow statement creates user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grant privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
