eb078d1ea19b38e109d7091cefb9368a0bb4b53b
Naod Taddese <naodtds@gmail.com>
2024-02-21 15:15:48 +0300

    task 8

--- models/user.py
+++ models/user.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base 
-from sqlalchemy import String, Column
+from models.base_model import BaseModel, Base
+import sqlalchemy
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    email = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
+    password = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
+    first_name = sqlalchemy.Column(sqlalchemy.String(128))
+    last_name = sqlalchemy.Column(sqlalchemy.String(128))
+
+    places = sqlalchemy.orm.relationship(
+        "Place", backref="user", cascade="all, delete-orphan")
