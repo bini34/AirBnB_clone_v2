fcfc48d337344c57ed88b3e62285ed1389402dd4
Naod Taddese <naodtds@gmail.com>
2024-02-21 15:47:41 +0300

    task 9 fix

--- models/review.py
+++ models/review.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import String, Foreignkey, Column
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, ForeignKey, Column
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(60), nullable=False)
+    text = Column(String(1024), nullable=False)
