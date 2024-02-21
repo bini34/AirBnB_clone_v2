aa1404053c95763cfac452ae9563f01773490c6b
Naod Taddese <naodtds@gmail.com>
2024-02-21 16:32:21 +0300

    fix typl

--- models/place.py
+++ models/place.py
@@ -11,7 +11,7 @@ place_amenity = Table(
     'place_amenity',
     Base.metadata,
     Column('place_id', String(60), ForeignKey('places.id'), nullable=False),
-    Column('amenity_id', String(60), ForeignKey('places.id'), nullable=False)
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False)
     )
 
 
