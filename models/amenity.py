8aab34f5a1942af41a0ee2f16ae6dbf4fa7b8ae9
Naod Taddese <naodtds@gmail.com>
2024-02-21 15:55:22 +0300

    task 10

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-
+from sqlalchemy import Column, String, relationship
 
 class Amenity(BaseModel, Base):
+    """blue print for amenity"""
     __tablename__ = 'amenities'
-    name = ""
+    name = Column(String(128), nullable=False)
 
