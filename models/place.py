fb74d059d98797b8ff9977fa94eb6bb6a7574842
Naod Taddese <naodtds@gmail.com>
2024-02-21 16:57:00 +0300

    final

--- models/place.py
+++ models/place.py
@@ -10,32 +10,39 @@ from sqlalchemy.orm import relationship
 place_amenity = Table(
     'place_amenity',
     Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'), nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False)
+    Column(
+        'place_id', String(60), ForeignKey('places.id'), nullable=False),
+    Column(
+        'amenity_id', String(60), ForeignKey('amenities.id'), nullable=False)
     )
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'places'
     ENV = os.getenv("HBNB_TYPE_STORAGE")
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    amenities = relationship(
-        'Amenity',
-        secondary=place_amenity,
-        viewonly=False,
-        backref='place_amenities')
+    if ENV == 'db':
+        __tablename__ = 'places'
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float)
+        longitude = Column(Float)
+        amenities = relationship(
+            'Amenity',
+            secondary=place_amenity,
+            viewonly=False,
+            backref='place_amenities')
+        reviews = relationship(
+            "Review", backref="place", cascade="all, delete-orphan")
 
-    if ENV != 'db':
+    else:
+        amenity_ids = []
+
         @property
         def reviews(self):
             """ge all review"""
@@ -53,6 +60,3 @@ class Place(BaseModel, Base):
                 for amenity in models.storage.all('Amenity').values()
                 if amienity.place_id == self.id
             ]
-    else:
-        reviews = relationship(
-            "Review", backref="place", cascade="all, delete-orphan")
