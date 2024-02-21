381a434541bb5ba8fd50c6d5741b6ebf709a1951
Naod Taddese <naodtds@gmail.com>
2024-02-21 13:15:53 +0300

    fixes on merge

--- models/amenity.py
+++ models/amenity.py
@@ -5,5 +5,5 @@ from models.base_model import BaseModel, Base
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
+    name = ""
 
--- models/review.py
+++ models/review.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel):
     __tablename__ = 'reviews'
     """ Review classto store review information """
-    place_id = Column(String(60), nullable=False, ForeignKey('places.id'))
-    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
-    text = Column(String(1024), nullable=False)
+    place_id = ""
+    user_id = ""
+    text = ""
