f0019c6607542d7ed0247f453c11bfe87b9f2cc9
Naod Taddese <naodtds@gmail.com>
2024-03-07 11:20:22 +0300

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 
 sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared/
 sudo echo "<html><head></head><body>Hello!</body></html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -14,7 +14,7 @@ sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '/^\s*server_name _;/a \
 \
-\tlocation /hbnb_static/ {\
+\tlocation /hbnb_static {\
 \t\talias /data/web_static/current/;\
 \t}' /etc/nginx/sites-available/default
 
