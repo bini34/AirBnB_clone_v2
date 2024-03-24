b120e0276bf4b92641dde6a44125429650df750c
bini34 <biniyamambachew@gmail.com>
2024-03-24 05:02:08 +0300

    add task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+
+from flask import Flask, render_template
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    function that return hello hbnb
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    function that return HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    function that return c
+    """
+    return f'C {escape(text.replace("_", " "))}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """
+    function that return python with input text
+    """
+    return f'Python {escape(text.replace("_", " "))}'
+
+
+@app.route('/python', strict_slashes=False)
+def python(text):
+    """
+    function that return Python is cool
+    """
+    return 'Python is cool'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    function that return is number
+    """
+    return f'{escape(n)} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    function that return n with template
+    """
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
