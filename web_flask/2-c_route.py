53d50e615377211b0befa2cf8478bcb4401b24e1
bini34 <biniyamambachew@gmail.com>
2024-03-24 03:41:42 +0300

    add task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    function that return hello hbnb
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    function that return HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """
+    function that return c
+    """
+    return f'C {escape(text.replace("_", " "))}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
